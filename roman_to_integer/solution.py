68f31f21f90a04a62a2bf171911a0f1455c9916f
Ped <96748874+pdrm26@users.noreply.github.com>
2025-01-29 01:10:32 +0330

    refactor: use slicing for better readability

--- roman_to_integer/solution.py
+++ roman_to_integer/solution.py
@@ -19,12 +19,12 @@ class Solution:
 
         i = 0
         while i < len(s):
-            if i + 1 < len(s) and s[i] + s[i + 1] in ["IV", 'IX', 'XL', "XC", 'CD', 'CM']:
-                num += roman_to_int_mapper.get(s[i] + s[i + 1], 0)
+            if  i + 1 < len(s) and s[i:i+2] in roman_to_int_mapper:
+                num += roman_to_int_mapper.get(s[i:i + 2] , 0)
                 i += 2
                 continue
 
             num += roman_to_int_mapper[s[i]]
             i += 1
-
+            
         return num
