f90177019355291451eb37703b0125926ef96ac4
pedram <peeeedii@gmail.com>
2025-01-29 01:03:14 +0330

    feat: solve the roman to integer puzzle with python

---
+++ roman_to_integer/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        num = 0
+        roman_to_int_mapper = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+
+        i = 0
+        while i < len(s):
+            if i + 1 < len(s) and s[i] + s[i + 1] in ["IV", 'IX', 'XL', "XC", 'CD', 'CM']:
+                num += roman_to_int_mapper.get(s[i] + s[i + 1], 0)
+                i += 2
+                continue
+
+            num += roman_to_int_mapper[s[i]]
+            i += 1
+
+        return num
