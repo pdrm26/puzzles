ba9934d0c158a187263092e10c08ddf9ff497bb7
pedram <peeeedii@gmail.com>
2025-04-17 23:21:57 +0330

    feat: solve the binary search puzzle

---
+++ binary-search/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            elif guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return -1
